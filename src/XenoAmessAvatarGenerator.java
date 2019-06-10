429e6684cc57c67261df6c1bbeca3da0f0977159
XenoAmess <xenoamess@gmail.com>
2019-06-11 00:36:10 +0800

    update

--- src/XenoAmessAvatarGenerator.java
+++ src/XenoAmessAvatarGenerator.java
@@ -1,8 +1,5 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.font.FontRenderContext;
-import java.awt.geom.Rectangle2D;
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -10,79 +7,80 @@ import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Map.Entry;
 
-import javax.imageio.ImageIO;
-
 public class XenoAmessAvatarGenerator {
-	public static String outputFileName = "output.png";
+    public static String outputFileName = "output2.png";
 
-	// output image's width and height(must be same).
-	public static int imageWidth = (1 << 15);
-	public static int imageHeight = imageWidth;
+    // output image's width and height(must be same).
+    public static int imageWidth = 3072;
+    public static int imageHeight = imageWidth;
 
-	// rate of the inner radius
-	public static double innerRadius = 0.5;
+    // rate of the inner radius
+    public static double innerRadius = 0.4;
 
-	// Color outof the circle
-	public static Color backgroundColor = new Color(0, 0, 0, 0);
+    // Color outof the circle
+    public static Color backgroundColor = new Color(0, 0, 0, 0);
 
-	// <Color,rate of the Color>
-	public static ArrayList<Entry<Color, Double>> colors = new ArrayList<Entry<Color, Double>>();
-	static {
-		colors.add(new AbstractMap.SimpleEntry<Color, Double>(new Color(255, 0, 0, 255), 0.33));
-		colors.add(new AbstractMap.SimpleEntry<Color, Double>(new Color(0, 0, 0, 255), 0.33));
-	}
+    // <Color,rate of the Color>
+    public static ArrayList<Entry<Color, Double>> colors = new ArrayList<Entry<Color, Double>>();
 
-	// Color of the plate of thevcircle
-	public static Color plateColor = new Color(255, 255, 255, 255);
+    static {
+        colors.add(new AbstractMap.SimpleEntry<Color, Double>(Color.red, 0.30));
+        colors.add(new AbstractMap.SimpleEntry<Color, Double>(Color.black, 0.30));
+    }
 
-	// repeat time for every colors.
-	public static int round = 3;
+    // Color of the plate of thevcircle
+    public static Color plateColor = new Color(255, 255, 255, 255);
 
-	public static void main(String args[]) {
+    // repeat time for every colors.
+    public static int round = 3;
 
-		File file = new File(outputFileName);
+    public static void main(String args[]) {
 
-		BufferedImage bi = new BufferedImage(imageWidth, imageWidth, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g2 = (Graphics2D) bi.createGraphics();
-		g2.setBackground(backgroundColor);
-		g2.clearRect(0, 0, imageWidth, imageHeight);
+        File file = new File(outputFileName);
 
-		g2.setColor(plateColor);
-		g2.fillOval(0, 0, imageWidth, imageHeight);
+        BufferedImage bi = new BufferedImage(imageWidth, imageWidth, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = (Graphics2D) bi.createGraphics();
+        g2.setBackground(backgroundColor);
+        g2.clearRect(0, 0, imageWidth, imageHeight);
 
-		double eachSideSize = 360.0 / round;
+        g2.setColor(plateColor);
+        g2.fillOval((int) (imageWidth * 0.01), (int) (imageHeight * 0.01), (int) (imageWidth * 0.98),
+                (int) (imageHeight * 0.98));
+//        g2.fillArc(0, 0, (int) (imageWidth * 0.99), (int) (imageHeight * 0.99), 0, 360);
 
-		double eachPlateSize = 1.0;
-		for (Entry<Color, Double> entry : colors) {
-			eachPlateSize -= entry.getValue();
-		}
-		double eachPlateAngle = eachPlateSize * eachSideSize / colors.size();
-		double nowAngle = 90 - eachSideSize * colors.get(0).getValue() / 2;
-		for (int i = 0; i < round; i++) {
-			for (Entry<Color, Double> entry : colors) {
-				System.out.println(nowAngle);
-				g2.setColor(entry.getKey());
-				// System.out.println(eachSideSize);
-				g2.fillArc(0, 0, imageWidth, imageHeight, (int) nowAngle, (int) (eachSideSize * entry.getValue()));
-				nowAngle += eachSideSize * entry.getValue();
-				System.out.println(nowAngle);
-				g2.setColor(plateColor);
-				g2.fillArc(0, 0, imageWidth, imageHeight, (int) nowAngle, (int) (eachPlateAngle));
-				nowAngle += eachPlateAngle;
-			}
+        double eachSideSize = 360.0 / round;
 
-		}
+        double eachPlateSize = 1.0;
+        for (Entry<Color, Double> entry : colors) {
+            eachPlateSize -= entry.getValue();
+        }
+        double eachPlateAngle = eachPlateSize * eachSideSize / colors.size();
+        double nowAngle = 90 - eachSideSize * colors.get(0).getValue() / 2;
+        for (int i = 0; i < round; i++) {
+            for (Entry<Color, Double> entry : colors) {
+                System.out.println(nowAngle);
+                g2.setColor(entry.getKey());
+                // System.out.println(eachSideSize);
+                g2.fillArc(0, 0, imageWidth, imageHeight, (int) nowAngle, (int) (eachSideSize * entry.getValue()));
+                nowAngle += eachSideSize * entry.getValue();
+                System.out.println(nowAngle);
+                g2.setColor(plateColor);
+                g2.fillArc(0, 0, imageWidth, imageHeight, (int) nowAngle, (int) (eachPlateAngle));
+                nowAngle += eachPlateAngle;
+            }
 
-		g2.setColor(plateColor);
-		g2.fillOval((int) (imageWidth - innerRadius * imageWidth) / 2,
-				(int) (imageHeight - innerRadius * imageHeight) / 2, (int) (innerRadius * imageWidth),
-				(int) (innerRadius * imageHeight));
+        }
 
-		try {
-			ImageIO.write(bi, "PNG", file);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        g2.setColor(plateColor);
+        g2.fillOval((int) (imageWidth - innerRadius * imageWidth) / 2,
+                (int) (imageHeight - innerRadius * imageHeight) / 2, (int) (innerRadius * imageWidth),
+                (int) (innerRadius * imageHeight));
+
+        try {
+            ImageIO.write(bi, "PNG", file);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
